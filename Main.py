74e34f4826e87207f3dad2115d56a20188342194
MohammedAAlbushaier <moh.albushaier@gmail.com>
2025-02-04 12:50:57 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,38 @@
+#Initialize a String Variable and assining the value to it 
+MyFavFottballTeam : str = "Barcelona FC"
+
+#Initialize a Intiger Variable and assining the value to it 
+x : int = 200
+
+#Initialize a float Variable and assining the value to it 
+y : float =2.25
+
+#Initialize multiple variables of primitive type at same time
+Moe1 = Moe2 = Moe3=1998
+
+#Initialize multiple iniger Variables and assining a diffrent values to them 
+team1, team2, team3=10, 20 , 30
+
+# to pring a String
+print (MyFavFottballTeam)
+
+#to print ann intiger
+print (x)
+
+#to print the type
+print  (type(y))
+
+#to print a float
+print (y)
+
+# to print multiple variables
+print (Moe1,Moe2,Moe3)
+
+# to print multiple variables
+print (team1,team2,team3)
+
+'''
+this is the first Python lab in the Course
+Feb4, 2025
+By Mohammed Albushaier
+'''
